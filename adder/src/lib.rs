e0053d6aaf7c361858aaeaa449b5f9bd578990a8
Julio Negri <julioyuki@outlook.com>
2022-05-31 09:27:16 -0300

    adding new test project with some examples about it

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -3,6 +3,15 @@ mod tests {
     use super::*;
 
     #[test]
+    fn two_plus_two() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("Two plus two doesn't equal to two"))
+        }
+    } 
+
+    #[test]
     fn add_two_test() {
         assert_eq!(4, add_two(2))
     }
@@ -20,6 +29,43 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn check_greetings() {
+       let result = greetings("Julio");
+       assert!(result.contains("Julio"), "Error found on check_greetings value got was: {}", result);
+    }
+
+    #[test]
+    #[should_panic(expected = "Num too big:")]
+    fn guess_panicking() {
+        Guess::new_value(200);
+    }
+
+    #[test]
+    #[should_panic(expected = "Num too small:")]
+    fn guess_small() {
+        Guess::new_value(-1);
+    }
+}
+
+fn greetings(name: &str) -> String {
+    format!("Greetings {}!", name)
+}
+
+struct Guess {
+    value: i32
+}
+
+impl Guess {
+    fn new_value(value: i32) -> Guess {
+        if value < 1  {
+            panic!("Num too small: {}", value)
+        } else if value > 100 {
+            panic!("Num too big: {}", value)
+        }
+        Guess { value }
     }
 }
 

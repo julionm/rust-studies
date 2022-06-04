773fae70866278a0f0322a679db08912b7372b9a
Julio Negri <julioyuki@outlook.com>
2022-06-04 05:39:18 -0300

    adding the new closures project with the implementations the book asked for

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ I commit myself to:
 
 # Rust Knowledge
 
-> Current Book Chapter: 12.6
+> Current Book Chapter: 13.2
 
 Some questions I asked myself, to keep track of what I was learning through the reading of ‘’The Rust Programming Language", available at: https://doc.rust-lang.org/book/.
 
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,14 +9,14 @@ fn main() {
     // * this err value is the one passed in Err call
     // * this closure is only executed if the Result is an Err
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // * as run doesn't return a value to unwrap
     // * we can use if let to return the error and exit
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
